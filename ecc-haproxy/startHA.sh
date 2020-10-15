fda55209842926b25b4675239286c9063d31f721
jobu <jonas.burster@inxmail.de>
2020-10-15 16:02:33 +0200

    working with test

--- ecc-haproxy/startHA.sh
+++ ecc-haproxy/startHA.sh
@@ -6,7 +6,24 @@ CONFIG_DIR="haconfig" && CONFIG_DIR=$REPO_DIR$CONFIG_DIR
 cd $REPO_DIR
 docker build -t haproxy .
 docker stop haproxy || true && docker rm haproxy || true
-echo "RUN"
-echo $CONFIG_DIR
+
 echo $CERTS_DIR
-docker run --name haproxy -p 9090:9090 -p 9091:9091 -v $CERTS_DIR:/etc/ssl -v $CONFIG_DIR:/usr/local/etc/haproxy:ro haproxy
+echo $CONFIG_DIR
+
+# For Windows  
+# Convert forward to backward slash -e 's/\//\\/g' 
+CERTS_DIR=$(echo "$CERTS_DIR" | sed -e 's/^\///' -e 's/^./\0:/')
+CONFIG_DIR=$(echo "$CONFIG_DIR" | sed -e 's/^\///' -e 's/^./\0:/')
+
+echo $CERTS_DIR
+echo $CONFIG_DIR
+
+echo "RUN"
+docker run --name haproxy -p 9090:9090 -p 9091:9091 -v $CERTS_DIR:/etc/ssl/certs -v $CONFIG_DIR:/usr/local/etc/haproxy:ro haproxy
+
+# check cert
+# echo | openssl s_client -showcerts -servername localhost -connect localhost:9091 2>/dev/null | openssl x509 -inform pem -noout -text
+# echo | openssl s_client -cipher 'AES256-SHA256' -showcerts -servername localhost -connect localhost:9091 2>/dev/null | openssl x509 -inform pem -noout -text
+
+# Analyse server.pem
+# openssl rsa -in certs/server.pem -text -noout
