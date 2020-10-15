fb7b9f7fb2dd91d3b7f5fa4e67ac3fbd39b8c178
jonas27 <jonas.burster@gmail.com>
2020-10-15 09:02:18 +0200

    ha ecc

---
+++ ecc-haproxy/make_certs/rsa/create.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+openssl genrsa -des3 -out rootCA.key 2048
+openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -out rootCA.pem
+
+# private key and cert
+openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout server.key
+openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 500 -sha256
+
+# combine into pem
+cat server.crt server.key | tee server.pem
--- ecc-haproxy/startHA.sh
+++ ecc-haproxy/startHA.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-docker build -t my-haproxy .
-docker run -d --name my-running-haproxy my-haproxy -p 9090:9090
+REPO="ecc-haproxy/"
+REPO_DIR=${PWD%ecc-haproxy*} && REPO_DIR="$REPO_DIR$REPO"
+CERTS_DIR="certs" && CERTS_DIR=$REPO_DIR$CERTS_DIR
+CONFIG_DIR="haconfig" && CONFIG_DIR=$REPO_DIR$CONFIG_DIR
+cd $REPO_DIR
+docker build -t haproxy .
+docker stop haproxy || true && docker rm haproxy || true
+echo "RUN"
+echo $CONFIG_DIR
+echo $CERTS_DIR
+docker run --name haproxy -p 9090:9090 -p 9091:9091 -v $CERTS_DIR:/etc/ssl -v $CONFIG_DIR:/usr/local/etc/haproxy:ro haproxy
