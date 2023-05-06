5b427ba280ca0d6b7b9eac708f57e58e018c633f
jonas27 <jonas.burster@gmail.com>
2023-05-06 22:42:12 +0200

    add spotifyd

---
+++ spofyd/rust/spotifyd/docs/src/SUMMARY.md
@@ -0,0 +1,17 @@
+# Spotifyd
+
+[Introduction](./Introduction.md)
+
+- [Installation Instructions](./installation/README.md)
+  - [Installing on a Raspberry Pi](./installation/Raspberry-Pi.md)
+  - [Installing on Ubuntu (from source)](./installation/Ubuntu.md)
+  - [Cross Compiling on Ubuntu](./installation/Cross-Compiling-on-Ubuntu.md)
+  - [Installing with Homebrew on macOS](./installation/MacOS.md)
+  - [Installing on FreeBSD](./installation/FreeBSD.md)
+- [Configuration](./config/README.md)
+  - [CLI options](./config/Cli.md)
+  - [Configuration file](./config/File.md)
+  - [Running as a Service](./config/services/README.md)
+    - [Running as systemd service](./config/services/Systemd.md)
+    - [Running as launchd service (MacOS)](./config/services/MacOS.md)
+
---
+++ spofyd/rust/spotifyd/src/main.rs
@@ -0,0 +1,112 @@
+#![cfg(unix)]
+
+use crate::config::CliConfig;
+use color_eyre::{
+    eyre::{self, eyre, Context},
+    Help, SectionExt,
+};
+use daemonize::Daemonize;
+use log::{error, info, trace, LevelFilter};
+use structopt::StructOpt;
+use tokio::runtime::Runtime;
+
+#[cfg(feature = "alsa_backend")]
+mod alsa_mixer;
+mod config;
+#[cfg(feature = "dbus_mpris")]
+mod dbus_mpris;
+mod error;
+mod main_loop;
+mod no_mixer;
+mod process;
+mod setup;
+mod utils;
+
+enum LogTarget {
+    Terminal,
+    Syslog,
+}
+
+fn setup_logger(log_target: LogTarget, verbose: bool) -> eyre::Result<()> {
+    let log_level = if verbose {
+        LevelFilter::Trace
+    } else {
+        LevelFilter::Info
+    };
+
+    let mut logger = fern::Dispatch::new().level(log_level);
+
+    if cfg!(feature = "dbus_mpris") && !verbose {
+        logger = logger.level_for("rspotify_http", LevelFilter::Warn);
+    }
+
+    let logger = match log_target {
+        LogTarget::Terminal => logger.chain(std::io::stdout()),
+        LogTarget::Syslog => {
+            let log_format = syslog::Formatter3164 {
+                facility: syslog::Facility::LOG_DAEMON,
+                hostname: None,
+                process: "spotifyd".to_owned(),
+                pid: 0,
+            };
+            logger.chain(
+                syslog::unix(log_format)
+                    .map_err(|e| eyre!("Couldn't connect to syslog instance: {}", e))?,
+            )
+        }
+    };
+
+    logger.apply().wrap_err("Couldn't initialize logger")
+}
+
+fn main() -> eyre::Result<()> {
+    color_eyre::install().wrap_err("Couldn't initialize error reporting")?;
+
+    let mut cli_config: CliConfig = CliConfig::from_args();
+
+    let is_daemon = !cli_config.no_daemon;
+
+    let log_target = if is_daemon {
+        LogTarget::Syslog
+    } else {
+        LogTarget::Terminal
+    };
+
+    setup_logger(log_target, cli_config.verbose)?;
+
+    cli_config
+        .load_config_file_values()
+        .wrap_err("could not load the config file")
+        .with_section(|| {
+            concat!(
+                "the config format should be valid TOML\n",
+                "we recently changed the config format, see https://github.com/Spotifyd/spotifyd/issues/765"
+            )
+            .header("note:")
+        })?;
+    trace!("{:?}", &cli_config);
+
+    // Returns the old SpotifydConfig struct used within the rest of the daemon.
+    let internal_config = config::get_internal_config(cli_config);
+
+    if is_daemon {
+        info!("Daemonizing running instance");
+
+        let mut daemonize = Daemonize::new();
+        if let Some(pid) = internal_config.pid.as_ref() {
+            daemonize = daemonize.pid_file(pid);
+        }
+        match daemonize.start() {
+            Ok(_) => info!("Detached from shell, now running in background."),
+            Err(e) => error!("Something went wrong while daemonizing: {}", e),
+        };
+    }
+
+    let runtime = Runtime::new().unwrap();
+    runtime.block_on(async {
+        let mut initial_state = setup::initial_state(internal_config);
+        initial_state.run().await;
+    });
+
+    Ok(())
+}
